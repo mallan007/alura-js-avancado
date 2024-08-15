db8303f69ad50f770ec645ef7af4b6a48e138248
Allan Queiroz Dev <allanqueiroz.eng@gmail.com>
2024-08-14 22:29:09 -0300

    (feature) Adicionado o modo de programação defensiva

--- client/js/app/models/Negociacao.js
+++ client/js/app/models/Negociacao.js
@@ -1,24 +1,24 @@
 class Negociacao { 
     constructor(data, quantidade, valor) { 
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-//Atributos privados são iniciados com _ e só podem ser acessados apenas dentro da classe
+        this._data = new Date(data.getTime());
+        this._quantidade = quantidade;//'this' acessa a instância que está sendo acessada  
+        this._valor = valor;//Atributos privados são iniciados com _ e só podem ser acessados apenas dentro da classe
+        Object.freeze(this); //Evitar que seja alterado em tempo de execução. Porém, ele não é Deep Freeze, apenas Shallow   
     }
-    obterVolume() {
-        return volume = this._quantidade * this._valor;
+    get volume() {
+        return this._quantidade * this._valor;
  
     }
 
-    getData() {
-        return this._data;
+    get data() {
+        return new Date(this._data.getTime());
     }
 
-    getQuantidade() {   
+    get quantidade() {   
         return this._quantidade;            
     }   
 
-    getValor() {    
+    get valor() {    
         return this._valor;
     }   
  
